96555bed5b2ec40dd3c99fc3ac9f29c4a7609c70
Yuri Kunde Schlesner <yuriks@yuriks.net>
2013-08-04 22:15:42 -0500

    Added matrix transform operator to Ray.

--- math/Ray.hpp
+++ math/Ray.hpp
@@ -1,9 +1,19 @@
 #pragma once
 #include "./vec.hpp"
+#include "./mat.hpp"
 
 namespace yks {
+
 	struct Ray {
 		vec3 origin;
 		vec3 direction;
 	};
+
+	inline Ray operator*(const mat4& m, const Ray& r) {
+		return Ray{
+			mvec3(m * mvec4(r.origin, 1.0f)),
+			mvec3(m * mvec4(r.direction, 0.0f))
+		};
+	}
+
 }

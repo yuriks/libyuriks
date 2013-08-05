d4eec6f07fdb7958f14512a59d1a5b4910736222
Yuri Kunde Schlesner <yuriks@yuriks.net>
2013-08-05 02:25:39 -0500

    Added Ray evaluation operator.

--- math/Ray.hpp
+++ math/Ray.hpp
@@ -7,6 +7,10 @@ namespace yks {
 	struct Ray {
 		vec3 origin;
 		vec3 direction;
+
+		inline vec3 operator()(float t) const {
+			return origin + direction*t;
+		}
 	};
 
 	inline Ray operator*(const mat4& m, const Ray& r) {

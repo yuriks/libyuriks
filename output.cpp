04a7f1e16dca7756fe511004fd38dca3cd240d60
Yuri Kunde Schlesner <yuriks@yuriks.net>
2013-08-05 12:50:13 -0500

    Clamp colors before converting to sRGB to prevent artifacts.

--- output.cpp
+++ output.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include "math/vec.hpp"
 #include "stb_image_write.h"
+#include "util.hpp"
 
 static float srgb_from_linear(float x) {
 	return x <= 0.0031308f ?
@@ -18,9 +19,9 @@ static uint8_t byte_from_linear(float x) {
 static std::vector<uint8_t> encode_srgb(const std::vector<yks::vec3> fdata) {
 	std::vector<uint8_t> bdata(fdata.size() * 3);
 	for (size_t i = 0; i < fdata.size(); ++i) {
-		bdata[i*3 + 0] = byte_from_linear(fdata[i][0]);
-		bdata[i*3 + 1] = byte_from_linear(fdata[i][1]);
-		bdata[i*3 + 2] = byte_from_linear(fdata[i][2]);
+		bdata[i*3 + 0] = byte_from_linear(clamp(0.0f, fdata[i][0], 1.0f));
+		bdata[i*3 + 1] = byte_from_linear(clamp(0.0f, fdata[i][1], 1.0f));
+		bdata[i*3 + 2] = byte_from_linear(clamp(0.0f, fdata[i][2], 1.0f));
 	}
 	return bdata;
 }

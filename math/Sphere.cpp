0830894c847537ad3e7f3c48c3502059838b18f9
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-02-23 22:46:09 -0300

    Discard intersections with t < 0 in intersect_with_sphere.

--- math/Sphere.cpp
+++ math/Sphere.cpp
@@ -5,19 +5,17 @@
 
 namespace yks {
 
-	Optional<float> intersect(const vec3& origin, float radius, const Ray& r) {
+	Optional<float> intersect_with_sphere(const vec3& origin, float radius, const Ray& r) {
 		const vec3 o = r.origin - origin;
 		const vec3 v = r.direction;
 
 		float t1, t2;
 		const int solutions = solve_quadratic(dot(v, v), 2*dot(o, v), dot(o, o) - radius*radius, t1, t2);
 
-		if (solutions == 0) {
+		if (solutions == 0 || t1 < 0.0f) {
 			return Optional<float>();
-		} else if (solutions == 1) {
-			return make_optional<float>(t1);
 		} else {
-			return make_optional<float>(std::min(t1, t2));
+			return make_optional<float>(t1);
 		}
 	}
 
--- math/Sphere.hpp
+++ math/Sphere.hpp
@@ -4,7 +4,7 @@
 #include "Optional.hpp"
 
 namespace yks {
-	Optional<float> intersect(const vec3& origin, float radius, const Ray& r);
+	Optional<float> intersect_with_sphere(const vec3& origin, float radius, const Ray& r);
 
 	/// Generates an uniformly distributed point on the surface of the unit
 	/// sphere centered on 0.  a and b are two numbers in [0,1).

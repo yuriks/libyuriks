33923bc44782dc88b4269e1f3b8a5ee6403fd417
Yuri Kunde Schlesner <yuriks@yuriks.net>
2014-03-29 18:22:05 -0300

    Make GL assert more debugger friendly

--- gl/gl_assert.hpp
+++ gl/gl_assert.hpp
@@ -2,9 +2,14 @@
 #include <cassert>
 #include "gl_1_5.h"
 
-#define YKS_CHECK_GL assert(glGetError() == GL_NO_ERROR)
+inline void check_gl_error() {
+	GLenum error = glGetError();
+	assert(error == GL_NO_ERROR);
+}
+
+#define YKS_CHECK_GL check_gl_error()
 #ifdef _DEBUG
-#define YKS_CHECK_GL_PARANOID assert(glGetError() == GL_NO_ERROR)
+#define YKS_CHECK_GL_PARANOID check_gl_error()
 #else
 #define YKS_CHECK_GL_PARANOID
 #endif
